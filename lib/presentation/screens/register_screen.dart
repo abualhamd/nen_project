2019fbbdd7bc734d3c3f3809e200e5e74260cb96
abualhamd <53705516+abualhamd@users.noreply.github.com>
2025-05-04 16:24:06 +0300

    feat: enhance RegisterScreen with BlocListener for registration state management

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -1,21 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nen_project/core/inputs/input_register.dart';
 
 import '../../app/app.dart' show RouteNames;
+import '../blocs.dart'
+    show
+        AuthBloc,
+        AuthState,
+        RegisterEvent,
+        RegisterLoadingState,
+        RegisterSuccessState;
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Register')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        switch (state) {
+          case RegisterSuccessState():
             context.goNamed(RouteNames.login);
-          },
-          child: const Text('Register'),
+            break;
+          default:
+            break;
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Register')),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              context.read<AuthBloc>().add(
+                RegisterEvent(
+                  input: InputRegister(
+                    name: "name",
+                    email: "email",
+                    password: "password",
+                  ),
+                ),
+              );
+            },
+            child: BlocSelector<AuthBloc, AuthState, bool>(
+              selector: (state) {
+                return state is RegisterLoadingState;
+              },
+              builder: (context, isLoading) {
+                return isLoading
+                    ? CircularProgressIndicator.adaptive()
+                    : const Text('Register');
+              },
+            ),
+          ),
         ),
       ),
     );
